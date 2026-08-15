4bedbe26bbdd77b109c3470a248607e9fd705d85
SHANYA984 <margaritabd902@gmail.com>
2026-08-15 01:25:26 -0300

    fix: remove duplicate service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -91,8 +91,4 @@ function App() {
   </div>;
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {}));
-}
-
 createRoot(document.getElementById('root')).render(<App />);
